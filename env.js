6d13debd905786459ba4fe8572653ee906a0fc4f
kt25730228 <kt25730228@gmail.com>
2025-12-02 10:05:26 +0900

    Add environment variables for Supabase

---
+++ env.js
@@ -0,0 +1,4 @@
+window.env = {
+    SUPABASE_URL: "https://lwwqzjoomrcswgyjkcjf.supabase.co",
+    SUPABASE_ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3d3F6am9vbXJjc3dneWprY2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1NzUxMDgsImV4cCI6MjA4MDE1MTEwOH0.NqH7wNOmfQKXN7AKT7H0zZsCvRUJC12Tc2jaCrN7_vw"
+};

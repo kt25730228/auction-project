defb3befc55724a7fecff2a6024a770c25ce0f77
kt25730228 <kt25730228@gmail.com>
2025-12-02 10:14:12 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
 const supabaseUrl = window.env.SUPABASE_URL;
 const supabaseAnonKey = window.env.SUPABASE_ANON;
 
+// Supabase 전역 객체 사용
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseAnonKey);
